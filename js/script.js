698416d7d03fe3194eca40c9b33b6b9e44d488fc
Alessandro <alessandrogattuso@yahoo.it>
2022-11-25 09:39:50 +0100

    remove backstrip

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let lastName = prompt('Qual è il tuo cognome'); //Last name
 let favColor = prompt('Quale è il tuo colore preferito?'); //Favorite color
 
 //Concatenate the input values and the number 21 inside var concat
-let concat = `${firstName}${lastName}${favColor}21`;
+let concat = firstName+lastName+favColor+21;
 
 //Write concat in the h1 with {id: 'password'} inside the html document
 document.getElementById('password').innerHTML = concat;
